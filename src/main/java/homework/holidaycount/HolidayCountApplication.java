30d6bd5337e66f5267c3192ac6522494efe781e2
Reinis Lubins <reinis.lubins@gmail.com>
2022-05-25 17:10:17 +0300

    first commit

---
+++ src/main/java/homework/holidaycount/HolidayCountApplication.java
@@ -0,0 +1,13 @@
+package homework.holidaycount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayCountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/homework/holidaycount/HolidayCountApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.holidaycount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayCountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
